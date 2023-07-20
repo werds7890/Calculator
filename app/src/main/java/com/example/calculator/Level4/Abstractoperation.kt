e5b24b87063c9b4be43c0f38e1b01f56532c7bb8
werds7890 <hihyungul@naver.com>
2023-07-20 11:12:49 +0900

    Calculator_Revision

--- app/src/main/java/com/example/calculator/Level4/Abstractoperation.kt
+++ app/src/main/java/com/example/calculator/Level4/Abstractoperation.kt
@@ -3,19 +3,4 @@ package com.example.calculator.Level4
 abstract class AbstractOperation() {
     abstract fun calculateNum() : String
 
-    fun addDescription() {
-        println("더하기 값 : ")
-    }
-
-    fun subDescription() {
-        println("빼기 값 : ")
-    }
-
-    fun mulDescription() {
-        println("곱하기 값 : ")
-    }
-
-    fun divDescription() {
-        println("나누기 값 : ")
-    }
 }
